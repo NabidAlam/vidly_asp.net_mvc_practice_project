66ca1e7601e4bfc0820835246610782a6914e727
Angie Siu <asiu@mscgroupny.com>
2016-10-17 11:29:21 -0400

    tested out various action results in MoviesController

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,11 +9,15 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie); //return a View
+            //return Content("hello world!"); //return the content, similar to Laravel's return $request->all();
+            //return HttpNotFound(); //return 404
+            //return new EmptyResult(); //return empty page
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" } ); //specify <ActionName, Controller, [{args to pass as URL query string}]> to redirect to
         }
     }
 }
