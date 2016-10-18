e2be45d73d4ca3312f6177dff925b58fcd02709b
Angie Siu <asiu@mscgroupny.com>
2016-10-18 15:39:41 -0400

    annotated lambda expression in CustomersController for further understanding of anon fcns, delegates, and lambda expressions

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -19,18 +19,18 @@ namespace Vidly.Controllers
             //"filter" that result of GetCustomers and obtain the Customer type object whose Id equals the id param
             var customer = GetCustomers().SingleOrDefault(cust => cust.Id == id);
 
-            /*
+            /******************
             //Alternate version A, using an explicitly stated param type in the lambda expression:
             var customer = GetCustomers().SingleOrDefault((Customer cust) => cust.Id == id);
-            */
-            
-            /*
+            ******************/
+
+            /******************
             //Alternal version B, using a delegate anonymous function;
             var customer = GetCustomers().SingleOrDefault(delegate(Customer cust)
             {
                 return cust.Id == id;
             });
-            */
+            ******************/
 
             if (customer == null)
             {
