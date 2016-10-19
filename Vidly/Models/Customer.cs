cd8e714fbcbc66c7b48a46ccb064561e51aeedb3
Angie Siu <asiu@mscgroupny.com>
2016-10-19 09:31:30 -0400

    test commit post repo import into github

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,5 +12,7 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+
+        //comment for test commit
     }
 }
