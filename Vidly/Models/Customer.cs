08cbd3d5583416dc33b588a3f6748ac603029c5a
Angie Siu <asiu@mscgroupny.com>
2016-10-21 14:42:46 -0400

    complete New customers form layout

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,9 +12,15 @@ namespace Vidly.Models
         [Required] //col no longer nullable
         [StringLength(255)] //specify max string length
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public string BirthDate { get; set; }
     }
 }
