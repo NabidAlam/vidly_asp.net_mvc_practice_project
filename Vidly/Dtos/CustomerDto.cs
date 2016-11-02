f1daa3445ce4626801349fe49bc5431452141ee9
Angie Siu <asiu@mscgroupny.com>
2016-11-02 14:27:53 -0400

    create CustomerDto for safer data transfer

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto { 
+        public int Id { get; set; }
+
+        [Required] //col no longer nullable and override default validation message
+        [StringLength(255)] //specify max string length
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; } //MembershipTypeId is required by default because type is byte, not nullable byte (i.e., byte?)
+
+        public string BirthDate { get; set; }
+    }
+}
