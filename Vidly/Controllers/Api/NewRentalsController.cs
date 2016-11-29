cd7063fa548028f85e7be4697e21158e6d8824e7
Angie Siu <asiu@mscgroupny.com>
2016-11-28 23:36:27 -0500

    rollback first 3 edge cases to depollute code using optimistic validation approach

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -20,30 +20,15 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto newRental)
         {
-            if (newRental.MovieIds.Count == 0)
-            {
-                return BadRequest("No Movie Ids have been given");
-            }
-
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
-
-            if(customer == null)
-            {
-                return BadRequest("Customer Id not valid");
-            }
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
 
             var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
-
-            if(movies.Count != newRental.MovieIds.Count)
-            {
-                return BadRequest("One ore more movie Ids are invalid");
-            }
 
             foreach (var movie in movies)
             {
                 if(movie.NumberAvailable == 0)
                 {
-                    return BadRequest("One or more MovieIds are not available");
+                    return BadRequest("One or more movies are not available");
                 }
                 movie.NumberAvailable--;
 
