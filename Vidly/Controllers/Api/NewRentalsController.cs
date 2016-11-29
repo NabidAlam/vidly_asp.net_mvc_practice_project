8683a35d0d7dfa403058a11641d684a1550fec03
Angie Siu <asiu@mscgroupny.com>
2016-11-28 23:33:01 -0500

    Use defensive approach to validate against edge cases in CreateNewRental: (1) No MovieIds were given or nothing was rented, (2) Invalid CustomerId, (3) One or more MovieIds are invalid, (4) One or more movies are not available

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -20,12 +20,31 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto newRental)
         {
-            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            if (newRental.MovieIds.Count == 0)
+            {
+                return BadRequest("No Movie Ids have been given");
+            }
 
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+
+            if(customer == null)
+            {
+                return BadRequest("Customer Id not valid");
+            }
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if(movies.Count != newRental.MovieIds.Count)
+            {
+                return BadRequest("One ore more movie Ids are invalid");
+            }
 
             foreach (var movie in movies)
             {
+                if(movie.NumberAvailable == 0)
+                {
+                    return BadRequest("One or more MovieIds are not available");
+                }
                 movie.NumberAvailable--;
 
                 var rental = new Rental
